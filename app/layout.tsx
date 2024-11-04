ebbf55cc71f6c84186f74d314db6af10feff68c0
andyvolk <volkov.andrey.a@gmail.com>
2024-11-05 07:13:48 +1000

    start
    https://github.com/talesmousinho/openai-assistant-file-search?tab=readme-ov-file

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import { IconGitHub, IconNextChat, IconSeparator } from '@/components/ui/icons'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Toaster } from '@/components/ui/toaster'
 import { Providers } from '@/components/providers'
 import './globals.css'
@@ -33,7 +34,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body className={cn('antialiased', inter.className)}>
         <Providers>
           <div className="flex min-h-screen flex-col">
-            <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between px-4 dark:bg-muted/50">
+            <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between bg-slate-100 px-4">
               <div className="flex items-center">
                 <Link href="/" rel="nofollow">
                   <IconNextChat className="mr-2 size-6 dark:hidden" inverted />
@@ -41,23 +42,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 </Link>
                 <div className="flex items-center">
                   <IconSeparator className="size-6 text-muted-foreground/50" />
-                  OpenAI Assistant File Search
+                  ERM Digital: AI-Enabled Accelerators
                 </div>
               </div>
               <div className="flex items-center justify-end space-x-2">
-                <a
-                  target="_blank"
-                  href="https://github.com/talesmousinho/openai-assistant-file-search"
-                  rel="noopener noreferrer"
-                  className={cn('dark:bg-transparent', buttonVariants({ variant: 'outline' }))}
-                >
-                  <IconGitHub />
-                  <span className="ml-2 hidden md:flex">GitHub</span>
-                </a>
+              <Avatar >
+  <AvatarImage src="" />
+  <AvatarFallback className="bg-green-400">CN</AvatarFallback>
+</Avatar>
               </div>
             </header>
-
-            <main className="flex flex-1 flex-col dark:bg-muted/50">{children}</main>
+            <div className="flex flex-row">
+              <div className="flex w-16 border-r-2 border-gray-400 bg-white ">test 123</div>
+              <main className="flex w-full bg-white">{children}</main>
+            </div>
           </div>
         </Providers>
         <Toaster />
